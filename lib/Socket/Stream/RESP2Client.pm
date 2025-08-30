59d348a2eb2f003b0dabc0a64daaf70ce0b4d8c4
Brett Watson <brett.watson@gmail.com>
2025-08-30 14:23:58 +1000

    Add nonblocking, callback-based response methods for Socket::Stream::RESP2Client

--- lib/Socket/Stream/RESP2Client.pm
+++ lib/Socket/Stream/RESP2Client.pm
@@ -17,6 +17,8 @@ use constant {
     TIMER   => 3,
 };
 
+sub NOOP { }
+
 # Arg is socket or host:port; default local Redis
 sub new {
     my ($class, $socket) = @_;
@@ -110,6 +112,105 @@ sub data_available {
     my ($self) = @_;
     $self->[STREAM]->recv_status; # obtain data if available
     return $self->[STREAM]->buffer_used;
+}
+
+### Receive data the hard way: non-blocking.
+
+# Callback when timer expires or ready to read socket.  Fails
+# immediately if recv stream has ended.
+sub io_cb {
+    my ($self, $okcb, $ngcb) = @_;
+    return $ngcb->("time limit or end of data")
+        if $self->[STREAM]->recv_end
+        or $self->[TIMER]->expired;
+    my ($t, $w);
+    # $next keeps $t and $w alive until called
+    my $next = sub { undef $t; undef $w; goto shift };
+    # $t and $w keep $next alive
+    $t = AnyEvent->timer(
+        after => $self->[TIMER]->remaining,
+        cb    => sub { $next->($ngcb, "timeout") },
+        ) if $self->[TIMER]->is_limited;
+    $w = AnyEvent->io(
+        fh   => $self->[SOCKET],
+        poll => 'r',
+        cb   => sub { $next->($okcb) },
+        );
+    return;
+}
+
+sub recv_empty_msg_cb {
+    my ($self, $data, $okcb, $ngcb) = @_;
+    my $msg = $self->[STREAM]->recv_msg_nb;
+    return $msg eq '' ? $okcb->($data) : $ngcb->("expected CRLF")
+        if defined $msg;
+    #@! Still awaiting bulk string terminator
+    return $self->io_cb(
+        sub { $self->recv_empty_msg_cb($data, $okcb, $ngcb) }, # retry
+        $ngcb,
+        );
+}
+
+sub recv_bulk_cb {
+    my ($self, $n, $okcb, $ngcb) = @_;
+    my $data = $self->[STREAM]->recv_data_nb($n);
+    if (defined $data) {
+        return $self->recv_empty_msg_cb($data, $okcb, $ngcb);
+    }
+    #@! recv_bulk_cb got no data
+    return $self->io_cb(
+        sub { $self->recv_bulk_cb($n, $okcb, $ngcb) }, # retry
+        $ngcb,
+        );
+}
+
+sub recv_array_cb {
+    my ($self, $n, $array, $okcb, $ngcb) = @_;
+    return $okcb->($array)
+        unless $n > 0;
+    return $self->recv_datum_cb(
+        sub {
+            push @$array, $_[0];
+            $self->recv_array_cb($n - 1, $array, $okcb, $ngcb);
+        },
+        $ngcb,
+        );
+}
+
+sub recv_datum_cb {
+    my ($self, $okcb, $ngcb) = @_;
+    my $data = $self->[STREAM]->recv_msg_nb;
+    if (defined $data) {
+        #@! get_datum_cb got '$data'
+        my $type = substr($data, 0, 1, '');
+        return $okcb->($data)
+            if $type eq '+' or $type eq ':';
+        return $okcb->(\$data)
+            if $type eq '-';
+        return $ngcb->("unrecognised data type")
+            unless $type eq '$' or $type eq '*';
+        return $okcb->(undef)
+            if $data eq '-1';
+        return $ngcb->("invalid count")
+            if $data =~ /\D/;
+        return $self->recv_bulk_cb($data, $okcb, $ngcb)
+            if $type eq '$';
+        #@! Start array size $data
+        return $self->recv_array_cb($data, [], $okcb, $ngcb);
+    }
+    #@! get_datum_cb got no data
+    return $self->io_cb(
+        sub { $self->recv_datum_cb($okcb, $ngcb) }, # retry
+        $ngcb,
+        );
+}
+
+sub response_cb {
+    my ($self, $okcb, $ngcb) = @_;
+    $okcb //= \&NOOP;
+    $ngcb //= \&NOOP;
+    $self->start_timer;
+    return $self->recv_datum_cb($okcb, $ngcb);
 }
 
 1;
