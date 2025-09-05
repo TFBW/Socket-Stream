a22812b6ad2bee70930c8d7b855a01dc146ec34b
Brett Watson <brett.watson@gmail.com>
2025-09-05 12:48:07 +1000

    Checkpoint RESP2Client: works, but has deep recursion

--- lib/Socket/Stream/RESP2Client.pm
+++ lib/Socket/Stream/RESP2Client.pm
@@ -15,9 +15,10 @@ use constant {
     STREAM  => 1,
     TIMEOUT => 2,
     TIMER   => 3,
+    PENDING => 4,
 };
 
-sub NOOP { }
+sub _NOOP { }
 
 # Arg is socket or host:port; default local Redis
 sub new {
@@ -31,14 +32,13 @@ sub new {
     }
     my $stream = Socket::Stream->new($socket)
         ->on_send_err(sub { die "Send error: $!\n" })
-        ->on_recv_err(sub { die "Recv error: $!\n" })
         ->use_CRLF;
     return bless([$socket, $stream], ref($class)||$class);
 }
 
 sub socket { $_[0][SOCKET] }
-sub timeout { @_ == 1 ? $_[0][TIMEOUT] : do { $_[0][TIMEOUT] = $_[1]; $_[0] } }
-sub start_timer { $_[0][TIMER] = time_left($_[0][TIMEOUT]) }
+sub timeout { time_left($_[0][TIMEOUT] = $_[1]); $_[0] }
+sub _start_timer { $_[0][TIMER] = time_left($_[0][TIMEOUT]) }
 
 # Send one or more strings as messages; can be used for inline style.
 sub request_raw {
@@ -57,7 +57,7 @@ sub request {
         );
 }
 
-sub recv_msg {
+sub _recv_msg {
     my ($self, $n) = @_;
     $self->[STREAM]->timeout($self->[TIMER]->remaining);
     return defined($n) ?
@@ -65,9 +65,9 @@ sub recv_msg {
         $self->[STREAM]->recv_msg;
 }
 
-sub recv_datum {
+sub _recv_datum {
     my ($self) = @_;
-    my $data = $self->recv_msg
+    my $data = $self->_recv_msg
         or die "Empty message\n";
     #@! Received msg '$data'
     my $type = substr($data, 0, 1, '');
@@ -81,14 +81,14 @@ sub recv_datum {
         if $data =~ /\D/;
     my $n = $data;
     if ($type eq '$') {
-        $data = $self->recv_msg($n);
+        $data = $self->_recv_msg($n);
         die "Bulk string parse error\n"
-            unless $self->recv_msg eq '';
+            unless $self->_recv_msg eq '';
     }
     else {
         #@! Start array size $n
         $data = [];
-        push @$data, $self->recv_datum # recurse
+        push @$data, $self->_recv_datum # recurse
             for 1..$n;
         #@! End array size $n
     }
@@ -97,8 +97,13 @@ sub recv_datum {
 
 sub response {
     my ($self) = @_;
-    $self->start_timer;
-    return $self->recv_datum;
+    die "Can't use response() while response_cb() requests in progress.\n"
+        if defined $self->[PENDING];
+    $self->[STREAM]->on_recv_err(sub { die "Recv error: $!\n" });
+    $self->_start_timer;
+    my $data = $self->_recv_datum;
+    $self->[STREAM]->on_recv_err();
+    return $data;
 }
 
 sub call {
@@ -116,68 +121,71 @@ sub data_available {
 
 ### Receive data the hard way: non-blocking.
 
-# Callback when timer expires or ready to read socket.  Fails
-# immediately if recv stream has ended.
-sub io_cb {
+# $okcb->() when ready to read socket; $ngcb->($reason) immediately if
+# recv stream has ended or time is up, later if timer expires.
+sub _io_cb {
     my ($self, $okcb, $ngcb) = @_;
-    return $ngcb->("time limit or end of data")
-        if $self->[STREAM]->recv_end
-        or $self->[TIMER]->expired;
+    return $ngcb->("end of data")
+        if $self->[STREAM]->recv_eof;
+    return $ngcb->($self->[STREAM]->recv_err)
+        if $self->[STREAM]->recv_err;
+    return $ngcb->("too late to wait")
+        if $self->[TIMER]->expired;
     my ($t, $w);
     # $next keeps $t and $w alive until called
-    my $next = sub { undef $t; undef $w; goto shift };
+    my $next = sub { undef $t; undef $w; shift->(@_) };
     # $t and $w keep $next alive
-    $t = AnyEvent->timer(
-        after => $self->[TIMER]->remaining,
-        cb    => sub { $next->($ngcb, "timeout") },
-        ) if $self->[TIMER]->is_limited;
-    $w = AnyEvent->io(
-        fh   => $self->[SOCKET],
-        poll => 'r',
-        cb   => sub { $next->($okcb) },
-        );
+    $t = AE::timer($self->[TIMER]->remaining, 0, sub { $next->($ngcb, "timeout") })
+        if $self->[TIMER]->is_limited;
+    $w = AE::io($self->[SOCKET], 0, sub { $next->($okcb) });
     return;
 }
 
-sub recv_empty_msg_cb {
+# $okcb->($data) when empty message received; $ngcb->($reason) when
+# non-empty message received or other error.
+sub _recv_empty_msg_cb {
     my ($self, $data, $okcb, $ngcb) = @_;
     my $msg = $self->[STREAM]->recv_msg_nb;
     return $msg eq '' ? $okcb->($data) : $ngcb->("expected CRLF")
         if defined $msg;
     #@! Still awaiting bulk string terminator
-    return $self->io_cb(
-        sub { $self->recv_empty_msg_cb($data, $okcb, $ngcb) }, # retry
+    return $self->_io_cb(
+        sub { $self->_recv_empty_msg_cb($data, $okcb, $ngcb) }, # retry
         $ngcb,
         );
 }
 
-sub recv_bulk_cb {
+# Reads $n bytes then passes to _recv_empty_msg_cb
+sub _recv_bulk_cb {
     my ($self, $n, $okcb, $ngcb) = @_;
     my $data = $self->[STREAM]->recv_data_nb($n);
-    if (defined $data) {
-        return $self->recv_empty_msg_cb($data, $okcb, $ngcb);
-    }
+    return $self->_recv_empty_msg_cb($data, $okcb, $ngcb)
+        if defined $data;
     #@! recv_bulk_cb got no data
-    return $self->io_cb(
-        sub { $self->recv_bulk_cb($n, $okcb, $ngcb) }, # retry
+    return $self->_io_cb(
+        sub { $self->_recv_bulk_cb($n, $okcb, $ngcb) }, # retry
         $ngcb,
         );
 }
 
-sub recv_array_cb {
+# Uses _recv_datum_cb to push $n items into $array, then calls
+# $okcb->($array).
+sub _recv_array_cb {
     my ($self, $n, $array, $okcb, $ngcb) = @_;
     return $okcb->($array)
         unless $n > 0;
-    return $self->recv_datum_cb(
+    return $self->_recv_datum_cb(
         sub {
             push @$array, $_[0];
-            $self->recv_array_cb($n - 1, $array, $okcb, $ngcb);
+            $self->_recv_array_cb($n - 1, $array, $okcb, $ngcb);
         },
         $ngcb,
         );
 }
 
-sub recv_datum_cb {
+# Read one datum then call $okcb->($datum); may use _recv_array_cb or
+# _recv_bulk_cv.  Calls $ngcb->($reason) on error.
+sub _recv_datum_cb {
     my ($self, $okcb, $ngcb) = @_;
     my $data = $self->[STREAM]->recv_msg_nb;
     if (defined $data) {
@@ -193,24 +201,181 @@ sub recv_datum_cb {
             if $data eq '-1';
         return $ngcb->("invalid count")
             if $data =~ /\D/;
-        return $self->recv_bulk_cb($data, $okcb, $ngcb)
+        return $self->_recv_bulk_cb($data, $okcb, $ngcb)
             if $type eq '$';
         #@! Start array size $data
-        return $self->recv_array_cb($data, [], $okcb, $ngcb);
+        return $self->_recv_array_cb($data, [], $okcb, $ngcb);
     }
     #@! get_datum_cb got no data
-    return $self->io_cb(
-        sub { $self->recv_datum_cb($okcb, $ngcb) }, # retry
+    return $self->_io_cb(
+        sub { $self->_recv_datum_cb($okcb, $ngcb) }, # retry
         $ngcb,
+        );
+}
+
+sub _fail_pending {
+    my ($self) = @_;
+    while (my $p = shift @{$self->[PENDING]}) {
+        $p->[1]->("");
+    }
+    undef $self->[PENDING];
+    return;
+}
+
+sub _next_pending {
+    my ($self) = @_;
+    if (@{$self->[PENDING]} == 0) {
+        undef $self->[PENDING];
+        return;
+    }
+    my ($okcb, $ngcb) = @{shift @{$self->[PENDING]}};
+    $self->_start_timer;
+    return $self->_recv_datum_cb(
+        sub { $okcb->(@_); $self->_next_pending }, # recurse
+        sub { $ngcb->(@_); $self->_fail_pending },
         );
 }
 
 sub response_cb {
     my ($self, $okcb, $ngcb) = @_;
-    $okcb //= \&NOOP;
-    $ngcb //= \&NOOP;
-    $self->start_timer;
-    return $self->recv_datum_cb($okcb, $ngcb);
+    $okcb //= \&_NOOP;
+    $ngcb //= \&_NOOP;
+    if ($self->[PENDING]) {
+        push @{$self->[PENDING]}, [$okcb, $ngcb];
+    }
+    else {
+        $self->[PENDING] = [[$okcb, $ngcb]];
+        $self->_next_pending;
+    }
+    return $self;
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Socket::Stream::RESP2Client - RESP2 (Redis) client using Socket::Stream
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 DESCRIPTION
+
+This is a simple low-level RESP2 (Redis) client interface based around
+L<Socket::Stream>.  It is low-level in that is has no knowledge of any
+Redis commands, only the RESP2 protocol which communicates commands
+and responses.
+
+Requests are always sent synchronously; responses may be received
+synchronously or, with L<AnyEvent>, asynchronously.  One distinctive
+feature of this module is the ability to pipeline commands: you can
+set up asynchronous response handlers in advance, then send the
+associated requests.
+
+=head1 METHODS
+
+The module is object-oriented and has the following methods.  Note
+that any exceptions or failures are generally unrecoverable: once you
+encounter one, you should dispose of the object and its socket rather
+than attempt further communication.
+
+=head2 new
+
+    $client = Socket::Stream::RESP2Client->new($socket);
+
+Creates a new $client object.  The $socket can be either an existing
+connected socket, or an INET "host:port" spec, in which case the host
+part defaults to 127.0.0.1 and the port defaults to 6379, the default
+Redis port.  If $socket is undef or empty, both defaults are used.  An
+exception is raised if socket connection fails.
+
+=head2 request
+
+    $client = $client->request(@args);
+
+Sends a request to the server in the usual "array of bulk strings"
+style.  The contents of @args should be byte-strings: any strings
+which may contain wide chars should be converted before sending.
+Blocking IO is used, but the event loop will run while waiting if
+L<AnyEvent> is in use.
+
+=head2 request_raw
+
+    $client = $client->request_raw(@strings);
+
+Sends one or more @strings, each followed by CRLF, to the server.  The
+request() method is written in terms of this, but you can use it to
+send commands in the "inline" style if you want to.
+
+=head2 response
+
+    $data = $client->response;
+
+Blocks and waits for one response, returning it as Perl data.  RESP2
+has data models for arrays, null, and various scalars.  Arrays are
+returned as ARRAY refs; null is returned as undef; integers and
+strings are returned as scalars.  Error strings are returned as scalar
+references to distinguish them from normal strings.  An exception is
+raised if there is any kind of communications or protocol error.
+
+=head2 call
+
+    $data = $client->call(@args);
+
+Shortcut for C<< $data = $client->request(@args)->response; >>.
+
+=head2 response_cb
+
+    $client->response_cb($okcb, $ngcb);
+
+Asynchronous response handling: requires L<AnyEvent>.  The arguments
+are CODE references to call back in case of success ($okcb) or failure
+($ngcb).  If either is undef, it is considered a no-op, but you'll
+generally want both.  The module will call C<< $okcb->($data) >> with
+$data as per the response() method, or C<< $ngcb->($reason) >> with
+$reason as an error message.  The callback will happen immediately if
+possible; otherwise it will be called from the event loop when ready.
+The callbacks should be exception-free because of the event loop.
+
+It is possible to queue response_cb() handlers: calling response_cb()
+again before the previous one is complete results in the callbacks
+being placed in a queue.  If any response results in a failure, any
+remaining items in the queue are failed immediately with empty string
+as the reason.
+
+Calls to response() are not permitted while response_cb() operations
+are in progress.
+
+=head2 timeout
+
+    $client = $client->timeout($duration);
+
+You can specify a time limit for request/response operations.  The
+default is undef, meaning no limit.  The duration can be set to a
+number of seconds or a time-unit string as accepted by to_seconds() in
+L<Time::Left>; invalid values will result in an exception.  Operations
+which exceed the time limit are aborted with an error.  In the case of
+response_cb(), the time limit applies to each response individually,
+representing the maximum wait for a callback once the operation is at
+the head of the queue.  If you want to impose a time limit on a group
+of operations, create a separate L<Time::Left> object or similar and
+dynamically adjust this timeout.
+
+=head2 data_available
+
+    $count = $client->data_available;
+
+Returns the number of bytes currently ready to read on the response
+side of the socket.  This can be used as a cheap alternative to full
+nonblocking operation: just do something else until data is available,
+then call response().
+
+=head2 socket
+
+    $socket = $client->socket;
+
+Provides access to the socket created at new().  If a connected socket
+was provided at new(), this returns the same socket.
+
